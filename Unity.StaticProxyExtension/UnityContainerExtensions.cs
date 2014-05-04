1826ffc2b462528601083dcc21ca2911ca8aeb36
Bruno Juchli <bruno.juchli@innocreative.ch>
2014-05-04 17:25:07 +0200

    updated StaticProxy.Interceptor

---
+++ Unity.StaticProxyExtension/UnityContainerExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Unity.StaticProxyExtension
+{
+    using Microsoft.Practices.Unity;
+    using Microsoft.Practices.Unity.Utility;
+
+    public static class UnityContainerExtensions
+    {
+        public static IUnityContainer RegisterInterfaceProxy<TInterface>(this IUnityContainer container, params InjectionMember[] injectionMembers)
+        {
+
+            Guard.ArgumentNotNull(container, "container");
+            return container.RegisterType(typeof(TFrom), typeof(TTo), null, null, injectionMembers);
+        }
+    }
+}
