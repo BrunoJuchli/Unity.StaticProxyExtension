6cb5fdd9c564c85542ae8013f22a6b68a81d0963
Bruno Juchli <bruno.juchli@innocreative.ch>
2016-04-10 14:52:05 +0200

    fixed assembly info to allow patching

--- Unity.StaticProxyExtension/Properties/AssemblyInfo.cs
+++ Unity.StaticProxyExtension/Properties/AssemblyInfo.cs
@@ -1,21 +1,15 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Unity.StaticProxyExtension")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Unity.StaticProxyExtension")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: NeutralResourcesLanguage("en")]
 
+// these will be set by the build server
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -28,3 +22,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
